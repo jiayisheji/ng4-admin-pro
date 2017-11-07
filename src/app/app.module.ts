057d0e1b34717074d0237dbaad39d6c0ccf28b55
690405541@qq.com <690405541@qq.com>
2017-11-07 19:46:26 +0800

    perf(根组件):  修改组件结构，配置全局路由，分离根模块依赖

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-
+import { CoreModule } from './core';
+import { AppRoutingModule } from './app-routing.module';
 @NgModule({
+  imports: [
+    BrowserModule,
+    CoreModule,
+    AppRoutingModule
+  ],
   declarations: [
     AppComponent
   ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
