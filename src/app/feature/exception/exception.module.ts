065b9292b7bbe29cdbb4e1eb2732dea871a34c5d
jiayi <690405541@qq.com>
2017-11-30 15:15:06 +0800

    perf(异常模块):  移动异常模块，403组件，404组件，500组件到特性模块下

--- src/app/feature/exception/exception.module.ts
+++ src/app/feature/exception/exception.module.ts
@@ -1,5 +1,5 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { SharedModule } from '@app/shared';
 
 import { ExceptionRoutingModule } from './exception-routing.module';
 import { NotFoundComponent } from './not-found/not-found.component';
@@ -8,7 +8,7 @@ import { InternalServerErrorComponent } from './internal-server-error/internal-s
 
 @NgModule({
   imports: [
-    CommonModule,
+    SharedModule,
     ExceptionRoutingModule
   ],
   declarations: [NotFoundComponent, ForbiddenComponent, InternalServerErrorComponent]
