07b271e005210fff3720a41cb92eff5512772355
jiayi <690405541@qq.com>
2017-12-04 18:09:34 +0800

    feat(异常模块):  新增异常模块下公共布局封装组件注册

--- src/app/feature/exception/exception.module.ts
+++ src/app/feature/exception/exception.module.ts
@@ -5,12 +5,13 @@ import { ExceptionRoutingModule } from './exception-routing.module';
 import { NotFoundComponent } from './not-found/not-found.component';
 import { ForbiddenComponent } from './forbidden/forbidden.component';
 import { InternalServerErrorComponent } from './internal-server-error/internal-server-error.component';
+import { ExceptionComponent } from './exception/exception.component';
 
 @NgModule({
   imports: [
     SharedModule,
     ExceptionRoutingModule
   ],
-  declarations: [NotFoundComponent, ForbiddenComponent, InternalServerErrorComponent]
+  declarations: [NotFoundComponent, ForbiddenComponent, InternalServerErrorComponent, ExceptionComponent]
 })
 export class ExceptionModule { }
